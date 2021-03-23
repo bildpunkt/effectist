0189f7dff1a020e20f4fce3b5f0914602e6814df
Andreas Nedbal <andy@pixelde.su>
2021-03-23 20:23:08 +0100

    :memo: document the Projects module

--- src/rest/projects.ts
+++ src/rest/projects.ts
@@ -1,9 +1,22 @@
 import RequestFactory from '../common/requestFactory';
 import { Project, ProjectCreateParameters, ProjectUpdateParameters } from './types';
 
+/**
+ * Class implementing methods to access Projects-related endpoints from the Todoist REST API
+ *
+ * @see {@link https://developer.todoist.com/rest/v1/#projects}
+ */
 export class Projects {
+  /**
+   * Instance of {@link RequestFactory}
+   */
   requestFactory: RequestFactory;
 
+  /**
+   * Class constructor
+   *
+   * @param accessToken - Access token required to authenticate against the REST API
+   */
   constructor(accessToken: string) {
     if (accessToken === undefined) {
       throw new Error('An access token must be passed to the constructor!');
@@ -15,28 +28,58 @@ export class Projects {
     );
   }
 
+  /**
+   * Returns all projects belonging to the authenticated user
+   *
+   * @returns an array of {@link Project} entries
+   */
   async findAll(): Promise<Project[]> {
     const response = await this.requestFactory.makeRequest();
 
     return response.json();
   }
 
+  /**
+   * Creates a new project for the authenticated user
+   *
+   * @param parameters - options defining how the project should be created
+   *
+   * @returns the created project
+   */
   async create(parameters: Partial<ProjectCreateParameters>): Promise<Project> {
     const response = await this.requestFactory.makePostRequest('', JSON.stringify(parameters));
 
     return response.json();
   }
 
+  /**
+   * Returns a project
+   *
+   * @param projectId - the ID of the project we want to get
+   *
+   * @returns the project with the given ID
+   */
   async get(projectId: number): Promise<Project> {
     const response = await this.requestFactory.makeRequest(`/${projectId}`);
 
     return response.json();
   }
 
+  /**
+   * Updates a project with new information
+   *
+   * @param projectId - the ID of the we want to get
+   * @param parameters - options defining how we want to update the project
+   */
   async update(projectId: number, parameters: Partial<ProjectUpdateParameters>): Promise<void> {
     await this.requestFactory.makePostRequest(`/${projectId}`, JSON.stringify(parameters));
   }
 
+  /**
+   * Deletes a project
+   *
+   * @param projectId - the ID of the project we want to delete
+   */
   async delete(projectId: number): Promise<void> {
     await this.requestFactory.makeDeleteRequest(`/${projectId}`);
   }
--- src/rest/types.ts
+++ src/rest/types.ts
@@ -1,12 +1,38 @@
 import { BaseProject } from '../common/types';
 
+/**
+ * Extended type for Projects returned by the REST API
+ *
+ * @see {@link https://developer.todoist.com/rest/v1/#projects}
+ */
 export type Project = BaseProject & {
+  /**
+   * Project position under the same parent (read-only).
+   */
   order: number;
+
+  /**
+   * Number of project comments.
+   */
   comment_count: number;
+
+  /**
+   * Whether the project is a favorite (a `true` or `false` value).
+   */
   favorite: boolean;
+
+  /**
+   * URL to access this project in the Todoist web or mobile applications.
+   */
   url: string;
 };
 
+/**
+ * Sub-type of {@link Project} defining the parameters for creating one
+ */
 export type ProjectCreateParameters = Pick<Project, 'name' | 'parent_id' | 'color' | 'favorite'>;
 
+/**
+ * Sub-type of {@link Project} defining the parameters for updating one
+ */
 export type ProjectUpdateParameters = Pick<Project, 'name' | 'color' | 'favorite'>;
